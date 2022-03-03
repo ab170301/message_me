b132fd32f8467f5abcd71d42b4b892e384ed3f65
ab170301 <antonia.blazevic173@gmail.com>
2022-03-03 20:08:39 +0100

    Added auto-scrolling to chat window

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,13 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start();
 
+// auto-scrolling in chatbox to the newst message
+window.scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
 
 $(document).on('turbolinks:load', function functionName() {
   $('.ui.dropdown').dropdown();
@@ -21,7 +28,8 @@ $(document).on('turbolinks:load', function functionName() {
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
-  
+
+  scroll_bottom();
 })
 
 
