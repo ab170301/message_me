98b8af1c718fe65ec3445ef88f84caa6e5e06f00
ab170301 <antonia.blazevic173@gmail.com>
2022-03-03 15:13:35 +0100

    Restrictions

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,5 @@
 class SessionController < ApplicationController
+   before_action :logged_in_redirect, only: [:new, :create]
 
   def new
 
@@ -20,6 +21,15 @@ class SessionController < ApplicationController
     session[:user_id] = nil
     flash[:success] = "You have successfully logged out"
     redirect_to login_path
+  end
+
+
+  private
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
   end
 
 end
