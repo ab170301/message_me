d9471bd1fb1168bcbb8f90d52b12fd824569eb88
ab170301 <antonia.blazevic173@gmail.com>
2022-03-03 14:54:13 +0100

    Added flash messages

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -8,10 +8,10 @@ class SessionController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] =  user.id
-      flash[:notice] = "Logged in successfully"
+      flash[:success] = "Logged in successfully"
       redirect_to root_path
     else
-      flash.now[:alert] = "There was something wrong with your login details."
+      flash.now[:error] = "There was something wrong with your login details."
       render 'new'
     end
   end
